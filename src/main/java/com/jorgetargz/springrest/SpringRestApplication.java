7e9f056fc699491414d886e175be533651c3a70b
Jorge Martín Llorente <jorgemartin99@outlook.com>
2023-02-08 02:18:37 +0100

    auto opening swagger ui when running Spring is ready

--- src/main/java/com/jorgetargz/springrest/SpringRestApplication.java
+++ src/main/java/com/jorgetargz/springrest/SpringRestApplication.java
@@ -4,6 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -12,7 +19,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRestApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.awt.headless", "false");
 		SpringApplication.run(SpringRestApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() throws URISyntaxException, IOException {
+		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+			Desktop.getDesktop().browse(new URI("http://localhost:8080/"));
+		}
 	}
 
 }
